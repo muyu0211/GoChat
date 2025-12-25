925d31d4bba0918c363783f2def9f05c69150774
Deng zengtao <dengzt11@163.com>
2025-12-25 15:18:06 +0800

    master

--- internel/model/dao/message.go
+++ internel/model/dao/message.go
@@ -1,16 +1,17 @@
 package dao
 
 import (
+	"time"
+
 	"go.uber.org/zap"
 	"gorm.io/gorm"
-	"time"
 )
 
 type MessageModel struct {
 	ID             uint64 `gorm:"primaryKey"` // 全局唯一 ID (自增或 Snowflake)
-	ConversationID string `gorm:"type:varchar(32);index:idx_conversation_id;not null;comment:会话id，使用会话双方id得到"`
+	ConversationID string `gorm:"type:varchar(32);index:idx_conversation_id;uniqueIndex:idx_conv_seq;not null;comment:会话id，使用会话双方id得到"`
 	// ClientMsgID    string `gorm:"type:varchar(64);"`          // 客户端生成的UUID (用于去重)
-	SeqID      uint64 `gorm:"index:idx_conversation_seq"` // 会话内序列号 (严格递增)
+	SeqID      uint64 `gorm:"index:idx_conversation_seq;uniqueIndex:idx_conv_seq"` // 会话内序列号 (严格递增)
 	SenderID   uint64 `gorm:"type:bigint;not null;comment:发送方id"`
 	ReceiverID uint64 `gorm:"type:bigint;not null;comment:接收方id（可以是user也可以是群）"`
 	Content    string `gorm:"type:text;not null;comment:存放信息内容；可以是图片、音频、视频的url"`
